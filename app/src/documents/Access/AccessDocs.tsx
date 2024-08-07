14a2fd7093edfc3b1bc0fead3e9a0b54a6cd5676
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-08-07 17:23:23 -0300

    feat: Se corrigen variables en Accesos

--- app/src/documents/Access/AccessDocs.tsx
+++ app/src/documents/Access/AccessDocs.tsx
@@ -20,6 +20,7 @@ const AccessDocs: React.FC = () => {
   const section3Ref = useRef<HTMLDivElement>(null);
   const section4Ref = useRef<HTMLDivElement>(null);
   const section5Ref = useRef<HTMLDivElement>(null);
+  const section6Ref = useRef<HTMLDivElement>(null);
 
   return (
     <BoxTemplate>
@@ -60,6 +61,27 @@ const AccessDocs: React.FC = () => {
                   >
                     <a href="#section-4">Con Lista</a>
                   </li>
+                  <li
+                    className={
+                      currentElementIndexInViewport === 3 ? "active" : ""
+                    }
+                  >
+                    <a href="#section-5">Con Lista</a>
+                  </li>
+                  <li
+                    className={
+                      currentElementIndexInViewport === 3 ? "active" : ""
+                    }
+                  >
+                    <a href="#section-6">Con Lista</a>
+                  </li>
+                  <li
+                    className={
+                      currentElementIndexInViewport === 3 ? "active" : ""
+                    }
+                  >
+                    <a href="#section-7">Con Lista</a>
+                  </li>
                 </ul>
               </div>
 
@@ -84,10 +106,10 @@ const AccessDocs: React.FC = () => {
                       <a href="" className="list-group-item item-sm">
                         Acceso
                       </a>
-                      <a href="" className="list-group-item item-sm hover">
+                      <a href="" className="list-group-item item-sm active">
                         Acceso
                       </a>
-                      <a href="" className="list-group-item item-sm active">
+                      <a href="" className="list-group-item item-sm hover">
                         Acceso
                       </a>
                       <a href="" className="list-group-item item-sm focus">
@@ -97,7 +119,7 @@ const AccessDocs: React.FC = () => {
                   </section>
 
                   {/* Con descripcion */}
-                  <section id="section-2" ref={section2Ref}>
+                  <section id="section-3" ref={section3Ref}>
                     <HeadingTemplate>
                       <h2>Con descripcion</h2>
                     </HeadingTemplate>
@@ -109,13 +131,13 @@ const AccessDocs: React.FC = () => {
                           <p className="access-text">Descripción (Opcional)</p>
                         </div>
                       </a>
-                      <a href="" className="list-group-item item-sm hover">
+                      <a href="" className="list-group-item item-sm active">
                         <div className="access-content">
                           <span className="access-title">Acceso</span>
                           <p className="access-text">Descripción (Opcional)</p>
                         </div>
                       </a>
-                      <a href="" className="list-group-item item-sm active">
+                      <a href="" className="list-group-item item-sm hover">
                         <div className="access-content">
                           <span className="access-title">Acceso</span>
                           <p className="access-text">Descripción (Opcional)</p>
@@ -131,7 +153,7 @@ const AccessDocs: React.FC = () => {
                   </section>
 
                   {/* Con icono */}
-                  <section id="section-2" ref={section2Ref}>
+                  <section id="section-4" ref={section4Ref}>
                     <HeadingTemplate>
                       <h2>Con icono</h2>
                     </HeadingTemplate>
@@ -144,14 +166,14 @@ const AccessDocs: React.FC = () => {
                           <p className="access-text">Descripción (Opcional)</p>
                         </div>
                       </a>
-                      <a href="" className="list-group-item item-sm hover">
+                      <a href="" className="list-group-item item-sm active">
                         <i className="bx bxs-info-circle"></i>
                         <div className="access-content">
                           <span className="access-title">Acceso</span>
                           <p className="access-text">Descripción (Opcional)</p>
                         </div>
                       </a>
-                      <a href="" className="list-group-item item-sm active">
+                      <a href="" className="list-group-item item-sm hover">
                         <i className="bx bxs-info-circle"></i>
                         <div className="access-content">
                           <span className="access-title">Acceso</span>
@@ -169,18 +191,43 @@ const AccessDocs: React.FC = () => {
                   </section>
 
                   {/* Tamaños */}
-                  <section id="section-2" ref={section2Ref}>
+                  <section id="section-5" ref={section5Ref}>
                     <HeadingTemplate>
                       <h2>Tamaños</h2>
                     </HeadingTemplate>
 
                     <div className="list-group">
                       <a href="" className="list-group-item ">
-                          <span className="access-title">Acceso</span>
+                        <span className="access-title">Acceso</span>
                       </a>
                       <a href="" className="list-group-item item-sm hover">
-                          <span className="access-title">Acceso</span>
+                        <span className="access-title">Acceso</span>
                       </a>
+                    </div>
+                  </section>
+
+                  {/* Lista ancho completo */}
+                  {/* Cuatro Columnas */}
+                  <section id="section-6" ref={section6Ref}>
+                    <HeadingTemplate>
+                      <h2>Cuatro Columnas</h2>
+                    </HeadingTemplate>
+
+                    <div className="container">
+                      <div className="access-deck max-cards-4">
+                        <a href="" className="list-group-item item-sm">
+                          <span className="access-title">Acceso</span>
+                        </a>
+                        <a href="" className="list-group-item item-sm">
+                          <span className="access-title">Acceso</span>
+                        </a>
+                        <a href="" className="list-group-item item-sm">
+                          <span className="access-title">Acceso</span>
+                        </a>
+                        <a href="" className="list-group-item item-sm">
+                          <span className="access-title">Acceso</span>
+                        </a>
+                      </div>
                     </div>
                   </section>
                 </div>

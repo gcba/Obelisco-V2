048bb3ec07048393640815fbaa2ec79ad6f8dabb
davisLjr <davisprojectjr@gmail.com>
2024-08-13 16:51:14 -0300

    fix: se añade documentacion y ruta a componentes formulario de textos

--- app/src/app/components/form-text/page.tsx
+++
@@ -1,6 +0,0 @@
-import FormTextDocs from "@/documents/Form-text/FormText";
-import React from "react";
-
-export default function CollapsePage () {
-  return <FormTextDocs />;
-}
--- app/src/documents/Form-text/FormText.tsx
+++ app/src/documents/Form-text/FormText.tsx
@@ -1,180 +1,131 @@
-'use client'
-
-import React, { useRef } from 'react';
-import { Scrollspy } from "@makotot/ghostui";
-import CodeCopy from "@/components/CodeBox";
-import BoxTemplate from '@/components/Template/Box';
-import HeadingTemplate from '@/components/Template/HeadingTemplate';
+import DocumentationTemplate from '@/components/Template/DocumentationTemplate';
+import CodeCopy from '@/components/CodeBox';
 import MainDescription from '@/components/Template/MainDescription';
-import Divisor from '@/components/Template/Divisor';
-import { EGcodeWithStrong, EGcodeSimpleAlert, EGcodeCloseAlert } from './code-views';
+
 
 const FormTextDocs: React.FC = () => {
+  const sections = [
+    {
+      id: 'section-1',
+      title: 'Formularios de texto',
+      content: (
+        <>
+          <MainDescription description="Ejemplos y pautas de uso para estilos de control de formulario, opciones de diseño y componentes personalizados para crear una amplia variedad de formularios." />
+        </>
+      ),
+      h1: true,
+    },
+    {
+      id: 'section-2',
+      title: 'Texto',
+      content: (
+        <>
+          <div className="">
+            <label htmlFor="exampleInputTexto" className="form-label">Texto</label>
+            <input type="text" className="form-control" id="exampleInputTexto" aria-describedby="Texto" placeholder="Textos" />
+            <div id="Texto" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          {/* <CodeCopy code={TEXT_THEME_COLORS} /> */}
+        </>
+      ),
+    },
+    {
+      id: 'section-3',
+      title: 'Número',
+      content: (
+        <>
+          <div className="">
+            <label htmlFor="exampleInputNumber" className="form-label">Número</label>
+            <input type="number" className="form-control" id="exampleInputNumber" aria-describedby="Number" placeholder="Números" />
+            <div id="Number" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          {/* <CodeCopy code={TEXT_THEME_COLORS} /> */}
+        </>
+      ),
+    },
+    {
+      id: "section-4",
+      title: "Correo electrónico",
+      content: (
+        <>
+          <div className="">
+            <label htmlFor="exampleInputEmail" className="form-label">Email</label>
+            <input type="email" className="form-control" id="exampleInputEmail" aria-describedby="Email" placeholder="Email@test.com" />
+            <div id="Email" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          {/* <CodeCopy code={TEXT_THEME_COLORS} /> */}
+        </>
+      ),
+    },
+    {
+      id: "section-5",
+      title: "Fecha",
+      content: (
+        <>
+          <div className="">
+            <label htmlFor="exampleInputDate" className="form-label">Fecha</label>
+            <input type="date" className="form-control" id="exampleInputDate" aria-describedby="Date" placeholder="Selecciona la fecha" />
+            <div id="Date" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          {/* <CodeCopy code={TEXT_THEME_COLORS} /> */}
+        </>
+      ),
+    },
+    {
+      id: "section-6",
+      title: "Bloque de texto",
+      content: (
+        <>
+          <div className="">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" />
+            <div id="Date" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          {/* <CodeCopy code={TEXT_THEME_COLORS} /> */}
+        </>
+      ),
+    },
+    {
+      id: "section-7",
+      title: "Deshabilitado",
+      content: (
+        <>
+          <div className="">
+            <label htmlFor="exampleInputTexto" className="form-label">Texto</label>
+            <input type="text" className="form-control" id="exampleInputTexto" aria-describedby="Texto" placeholder="Textos" disabled />
+            <div id="Texto" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
 
-  const section1Ref = useRef<HTMLDivElement>(null);
-  const section2Ref = useRef<HTMLDivElement>(null);
-  const section3Ref = useRef<HTMLDivElement>(null);
-  const section4Ref = useRef<HTMLDivElement>(null);
-  const section5Ref = useRef<HTMLDivElement>(null);
+          <div className="">
+            <label htmlFor="exampleInputNumber" className="form-label">Número</label>
+            <input type="number" className="form-control" id="exampleInputNumber" aria-describedby="Number" placeholder="Números" disabled />
+            <div id="Number" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+
+          <div className="">
+            <label htmlFor="exampleInputEmail" className="form-label">Email</label>
+            <input type="email" className="form-control" id="exampleInputEmail" aria-describedby="Email" placeholder="Email@test.com" disabled />
+            <div id="Email" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+
+          <div className="">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+            <input type="date" className="form-control" id="exampleInputEmail" aria-describedby="Date" placeholder="selecciona tu fecha" disabled />
+            <div id="Date" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+
+          <div className="">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" disabled />
+          </div>
+          {/* <CodeCopy code={TEXT_THEME_COLORS} /> */}
+        </>
+      ),
+    },
+  ];
 
   return (
-    <BoxTemplate>
-      <div
-        className="col-12"
-      >
-        <Scrollspy sectionRefs={[
-          section1Ref,
-          section2Ref,
-          section3Ref,
-          section4Ref,
-        ]}>
-          {({ currentElementIndexInViewport }) => (
-            <div className='box-scrollspy'>
-              <div className='nav-scrollspy d-none d-lg-block'>
-                <ul
-                  className='scrollspy'
-                  data-cy="nav-wrapper"
-                >
-                  <p className='font-weight-bold mb-1'> On this page
-                  </p>
-                  <li className={currentElementIndexInViewport === 0 ? "active" : ""}><a href="#section-1">Introducción</a></li>
-                  <li className={currentElementIndexInViewport === 1 ? "active" : ""}><a href="#section-2">Simple</a></li>
-                  <li className={currentElementIndexInViewport === 2 ? "active" : ""}><a href="#section-3">Con cierre</a></li>
-                  <li className={currentElementIndexInViewport === 3 ? "active" : ""}><a href="#section-4">Con Lista</a></li>
-                </ul>
-              </div>
-
-              <article className="box-content col-12 col-lg-8">
-                <div data-cy="section-wrapper">
-                  <section id="section-1" ref={section1Ref}>
-                    <HeadingTemplate>
-                      <h1>Formulario de textos</h1>
-                    </HeadingTemplate>
-                    <MainDescription description="Las alertas son avisos que mantienen a los usuarios informados sobre el estado del sistema y pueden requerir o no que realicen una acción." />
-                  </section>
-
-                  <Divisor />
-
-                  <section id="section-2" ref={section2Ref}>
-                    <HeadingTemplate>
-                      <h2 className="mb-4">Texto</h2>
-                    </HeadingTemplate>
-
-                    <div className="col-12 col-lg-8">
-                      <div className="mb-3">
-                        <label htmlFor="exampleFormControlInput1texts" className="form-label disabled">text</label>
-                        <input type="text" className="form-control" id="exampleFormControlInput1texts" placeholder="texto" disabled />
-                      </div>
-                      <div className="mb-3">
-                        <label htmlFor="exampleFormControlInput1number" className="form-label">numero</label>
-                        <input type="number" className="form-control" id="exampleFormControlInput1number" placeholder="numero" />
-                      </div>
-                      <div className="mb-3">
-                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                      </div>
-                      <div className="mb-3">
-                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows={3}></textarea>
-                      </div>
-                    </div>
-
-                    <Divisor />
-
-                    <label htmlFor="inputPassword5" className="form-label">Password</label>
-                    <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" />
-                    <div id="passwordHelpBlock" className="form-text">
-                      Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
-                    </div>
-
-                    <Divisor />
-
-                    <div className="mb-3">
-                      <label htmlFor="exampleFormControlInput1date" className="form-label">Select date</label>
-                      <input type="date" className="form-control" id="exampleFormControlInput1date" placeholder="seleccionar fecha" />
-                    </div>
-
-                    <CodeCopy code={EGcodeSimpleAlert} />
-                  </section>
-
-                  <Divisor />
-
-                  <section id="section-3" ref={section3Ref}>
-                    <HeadingTemplate>
-                      <h2 className="mb-4">Tamaños</h2>
-                    </HeadingTemplate>
-
-                    <div className="w-50 d-flex flex-column gap-2 mb-3">
-                      <input className="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example" />
-                      <input className="form-control" type="text" placeholder="Default input" aria-label="default input example" />
-                      <input className="form-control form-control-sm" type="text" placeholder=".form-control-sm" aria-label=".form-control-sm example" />
-                    </div>
-
-                    <CodeCopy code={EGcodeCloseAlert} />
-                  </section>
-
-                  <Divisor />
-
-                  <section id="section-4" ref={section4Ref}>
-                    <HeadingTemplate>
-                      <h3 className="mb-4">Mail</h3>
-                    </HeadingTemplate>
-
-                    <div className="mb-3">
-                      <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                      <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-                    </div>
-
-                    <br />
-
-                    <CodeCopy code={EGcodeWithStrong} />
-                  </section>
-
-                  <Divisor />
-
-                  <section id="section-5" ref={section5Ref}>
-                    <HeadingTemplate>
-                      <h3 className="mb-4">Validación</h3>
-                    </HeadingTemplate>
-
-                    <div className="w-50 d-flex flex-column gap-2 mb-3 mx-auto">
-                      <div className="alert alert-danger" role="alert">
-                        <p>
-                          <strong>Este es un destacado de una alerta de error.</strong>
-                          Esta es la descripción de una alerta de error que continua al texto
-                          destacado.
-                        </p>
-                        <ol className="list-links">
-                          <li>
-                            <a href="#">Ancla al error 1</a>
-                          </li>
-                          <li>
-                            <a href="#">Ancla al error 2</a>
-                          </li>
-                          <li>
-                            <a href="#">Ancla al error 3</a>
-                          </li>
-                          <li>
-                            <a href="#">Ancla al error 4</a>
-                          </li>
-                          <li>
-                            <a href="#">Ancla al error 5</a>
-                          </li>
-                        </ol>
-                      </div>
-                    </div>
-
-                    <br />
-
-                    <CodeCopy code={EGcodeWithStrong} />
-                  </section>
-                </div>
-              </article>
-            </div>
-          )}
-        </Scrollspy>
-      </div>
-    </BoxTemplate >
+    <DocumentationTemplate sections={sections} />
   );
 };
 

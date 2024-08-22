95e62d07974336454dcdac1d99deb4b6468c3ebf
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-08-22 11:14:27 -0300

    feat: Se agrega la ruta para visualizar Accesos

--- app/src/components/Layout/main-layout.tsx
+++ app/src/components/Layout/main-layout.tsx
@@ -30,6 +30,8 @@ export const componentsPages = [
   { text: "Switch", url: "/components/switch", id: 22 },
   { text: "Tooltip", url: "/components/tooltip", id: 23 },
   { text: "Tipografia", url: "/components/typography", id: 24 },
+  { text: "Accesos", url: "/components/access", id: 25 },
+
 ];
 
 export const gettingPages = [

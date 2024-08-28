0bf7d17dcfdfb72bec6a922a444549c7a2c489c1
davisLjr <davisprojectjr@gmail.com>
2024-08-28 17:37:35 -0300

    fix: se añade temporalmente accesos a la page de components, estos luegos seran remplazados por las cards

--- app/src/app/components/page.tsx
+++ app/src/app/components/page.tsx
@@ -1,9 +1,25 @@
-import BoxTemplate from "@/components/Template/Box";
+import Link from "next/link";
+
+import { componentNames } from "./[componentName]/generateStaticParams";
 
 export default function ComponentsPage() {
   return (
-    <BoxTemplate>
-      <h1>Aca colocar un mapeado de tarjetas con los componentes</h1>
-    </BoxTemplate>
+    <>
+      <div className="access-items-3">
+        {componentNames.map((name, index) => (
+          <div className="col" key={index}>
+            <Link href={`components/${name}`} className="list-group-item item-sm">
+              <span className="material-symbols-rounded">
+                code_blocks
+              </span>
+              <div className="access-content">
+                <span className="access-title">{name}</span>
+                <p className="access-text">Componente</p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }

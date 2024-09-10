836303ad0408558e61d22fbff679f6b5ea2024d9
davisLjr <davisprojectjr@gmail.com>
2024-09-10 14:47:58 -0300

    docs: prueba next config

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -3,9 +3,7 @@ const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true
-    },
-    basePath: '/Obelisco-V2',
-    assetPrefix: '/Obelisco-V2',
+    }
 };
 
 export default nextConfig;

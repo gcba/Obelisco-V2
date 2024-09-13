6fffd8b7c3a9c41a737a0c655149ef2bfcd3d236
Victoria Nastasi <victorianastasi@gmail.com>
2024-09-13 15:49:08 -0300

    fix: se agrega url de estandares

--- app/src/documents/Header/HeaderDocs.tsx
+++ app/src/documents/Header/HeaderDocs.tsx
@@ -14,7 +14,15 @@ const HeaderDocs: React.FC = () => {
       title: 'Header (encabezado)',
       h1: true,
       content: (
-        <MainDescription description="El header es una sección ubicada en la parte superior de la página que contiene elementos y componentes relacionados a la navegación. Su función es permitir que la persona usuaria pueda navegar y acceder intuitivamente a las diferentes secciones de un sitio." />
+        <>
+          <MainDescription description="El header es una sección ubicada en la parte superior de la página que contiene elementos y componentes relacionados a la navegación. Su función es permitir que la persona usuaria pueda navegar y acceder intuitivamente a las diferentes secciones de un sitio." />
+          <p className="lead mb-4">
+            Si quieres conocer las buenas prácticas de uso del componente, puedes visitar el siguiente&nbsp;
+            <a href="https://gcba.github.io/estandares/componentes/encabezado/" target="_blank" rel="noopener noreferrer">
+              enlace
+            </a>.
+          </p>
+        </>
       ),
     },
     {

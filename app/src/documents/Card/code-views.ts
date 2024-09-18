2ae6b21f1ec4fe174c88469445ad52386a1c82d9
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-09-18 13:55:16 -0300

    feat: Se agregan Code Views. Clase unbordered funciona correctamente.
    Falta: - Chequear punto de corte de Disposicion de Tarjetas, - Desplazable no funciona correctamente

--- app/src/documents/Card/code-views.ts
+++ app/src/documents/Card/code-views.ts
@@ -1,3 +1,198 @@
-export const CARDS = `
-    lorem
+// HORIZONTALES
+export const HORIZONTAL_ICON = `
+<div class="card card-horizontal">
+<i class="bx bxs-info-circle card-icon-left"></i>
+<div class="card-body">
+    <h5 class="card-title">
+    <a href="#" class="card-title-link">
+        Título de la tarjeta
+    </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+</div>
+</div>
+
+<div class="card card-horizontal unbordered">
+<i class="bx bxs-info-circle card-icon-left"></i>
+<div class="card-body">
+    <h5 class="card-title">
+    <a href="#">
+        Título de la tarjeta
+    </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+</div>
+</div>
 `;
+
+export const HORIZONTAL_IMG = `
+<div class="card card-horizontal">
+<img src="" class="card-img-left" alt="Img GCBA" />
+<div class="card-body">
+    <h5 class="card-title">
+    <a href="#" class="card-title-link">
+        Título de la tarjeta
+    </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+</div>
+</div>
+
+<div class="card card-horizontal unbordered">
+<img src="" class="card-img-left" alt="Img GCBA" />
+<div class="card-body">
+    <h5 class="card-title">
+    <a href="#">
+        Título de la tarjeta
+    </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+</div>
+</div>
+`;
+
+export const HORIZONTAL_TEXT = `
+<div class="card card-horizontal">
+<div class="card-body">
+    <h5 class="card-title">
+    <a href="#" class="card-title-link">
+        Título de la tarjeta
+    </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+</div>
+</div>
+
+<div class="card card-horizontal unbordered">
+<div class="card-body">
+    <h5 class="card-title">
+    <a href="#">
+        Título de la tarjeta
+    </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+</div>
+</div>
+`;
+
+export const HORIZONTAL_THEME = `
+<div class="card card-horizontal unbordered">
+    <img class="card-img-left" src="" alt="Imagen Tematica" />
+    <div class="card-body">
+    <h5 class="card-title">
+        <a href="#">
+        Título de la tarjeta con más de una línea
+        </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+    </div>
+</div>
+`;
+// VERTICALES
+
+export const VERTICAL_ICON = `
+<div class="card">
+    <div class="card-body">
+    <i class="bx bxs-info-circle card-icon"></i>
+    <h5 class="card-title">
+        <a href="#" class="card-title-link">
+        Título de la tarjeta
+        </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+    </div>
+    </div>
+</div>
+
+<div class="card card-simple unbordered">
+    <div class="card-body">
+    <i class="bx bxs-info-circle card-icon"></i>
+    <h5 class="card-title">
+        <a href="#" class="card-title-link">
+        Título de la tarjeta
+        </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+    </div>
+    </div>
+</div>
+`;
+
+export const VERTICAL_IMG = `
+<div class="card">
+    <div class="card-body">
+    <img class="card-img-top" src="" alt="Imagen Tarjeta Noticia" />
+    <h5 class="card-title">
+        <a href="#" class="card-title-link">
+        Título de la tarjeta
+        </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+    </div>
+    </div>
+</div>
+`;
+
+export const VERTICAL_NOTICE = `
+<div class="card">
+    <img class="card-img-top" src="" alt="Imagen Tarjeta Noticia" />
+    <div class="card-body">
+    <div class="card-badges">
+        <span class="badge badge-default">etiqueta 1</span>
+        <span class="badge badge-default">etiqueta 2</span>
+    </div>
+    <h5 class="card-title">
+        <a href="#" class="card-title-link">
+        Título de la tarjeta
+        </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+    <div class="card-info">
+        <div class="pt-2">
+        <small>
+            <span>Fecha de publicación</span>
+        </small>
+        </div>
+    </div>
+    </div>
+</div>
+`;
+
+export const VERTICAL_AGENDA = `
+<div class="card">
+    <img class="card-img-top" src="https://gcba.github.io/Obelisco/cards/img-top.jpg" alt="Imagen Tarjeta Noticia" />
+    <div class="card-body">
+    <p class="card-headline">sobrelinea</p>
+    <h5 class="card-title">
+        <a href="#" class="card-title-link">
+        Título de la tarjeta
+        </a>
+    </h5>
+    <p class="card-text">Descripción de la tarjeta</p>
+    <div class="card-info">
+        <div class="pt-2">
+        <small>
+            <span class="material-symbols-rounded">
+            calendar_today
+            </span>
+            Lun 01/1
+        </small>
+        <small>
+            <span class="material-symbols-rounded">
+            schedule
+            </span>
+            17 | 18 | 19 hs
+        </small>
+        </div>
+    </div>
+    </div>
+</div>
+`;
+
+export const DISPOSITION_3 = `
+`;
+
+export const DISPOSITION_2 = `
+`;
+
+// DISPOSICIÓN

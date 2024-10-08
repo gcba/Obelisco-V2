cb091e0621a7b5795e9509da9500638a35aebd30
Victoria Nastasi <victorianastasi@gmail.com>
2024-10-08 14:08:42 -0300

    fix: prettier

--- app/src/documents/AccessOrganism/AccessOrganismDoc.tsx
+++ app/src/documents/AccessOrganism/AccessOrganismDoc.tsx
@@ -6,11 +6,7 @@ import CodeBox from '@/components/CodeBox';
 import DocumentationTemplate from '@/components/Template/DocumentationTemplate';
 import MainDescription from '@/components/Template/MainDescription';
 
-import {
-  CATEGORIES_ACCESS_ORGANISM,
-  LIST_ACCESS_ORGANISM,
-  SIMPLE_ACCESS_ORGANISM,
-} from './code-views';
+import { CATEGORIES_ACCESS_ORGANISM, LIST_ACCESS_ORGANISM, SIMPLE_ACCESS_ORGANISM } from './code-views';
 
 const AccessOrganismDocs: React.FC = () => {
   const sections = [
@@ -47,10 +43,14 @@ const AccessOrganismDocs: React.FC = () => {
                               <h4 className="mb-1">Título del acceso</h4>
                               <p className="mb-0">Descripción del acceso</p>
                             </div>
-                            <span className="card-access-icon material-symbols-rounded" aria-hidden="true">info</span>
+                            <span className="card-access-icon material-symbols-rounded" aria-hidden="true">
+                              info
+                            </span>
                           </div>
                           <a href="#" className="list-group-item card-item">
-                            <span className="material-symbols-rounded o-icon" aria-hidden="true">info</span>
+                            <span className="material-symbols-rounded o-icon" aria-hidden="true">
+                              info
+                            </span>
                             <div className="access-content">
                               <span className="access-title">Acceso</span>
                               <p className="access-text">Descripción (Opcional)</p>
@@ -91,21 +91,27 @@ const AccessOrganismDocs: React.FC = () => {
                           <div className="container-access">
                             <div className="access-option">
                               <a href="#" className="list-group-item item-sm">
-                                <span className="material-symbols-rounded o-icon" aria-hidden="true">info</span>
+                                <span className="material-symbols-rounded o-icon" aria-hidden="true">
+                                  info
+                                </span>
                                 <div className="access-content">
                                   <span className="access-title">Acceso</span>
                                   <p className="access-text">Descripción (Opcional)</p>
                                 </div>
                               </a>
                               <a href="#" className="list-group-item item-sm">
-                                <span className="material-symbols-rounded o-icon" aria-hidden="true">info</span>
+                                <span className="material-symbols-rounded o-icon" aria-hidden="true">
+                                  info
+                                </span>
                                 <div className="access-content">
                                   <span className="access-title">Acceso</span>
                                   <p className="access-text">Descripción (Opcional)</p>
                                 </div>
                               </a>
                               <a href="#" className="list-group-item item-sm">
-                                <span className="material-symbols-rounded o-icon" aria-hidden="true">info</span>
+                                <span className="material-symbols-rounded o-icon" aria-hidden="true">
+                                  info
+                                </span>
                                 <div className="access-content">
                                   <span className="access-title">Acceso</span>
                                   <p className="access-text">Descripción (Opcional)</p>
@@ -115,7 +121,9 @@ const AccessOrganismDocs: React.FC = () => {
                           </div>
                           <hr className="clearfix" />
                           <a href="#" className="list-group-item card-item">
-                            <span className="material-symbols-rounded o-icon" aria-hidden="true">info</span>
+                            <span className="material-symbols-rounded o-icon" aria-hidden="true">
+                              info
+                            </span>
                             <div className="access-content">
                               <span className="access-title">Acceso grande</span>
                             </div>
@@ -156,13 +164,17 @@ const AccessOrganismDocs: React.FC = () => {
                             <div className="access-option">
                               <h5 className="title-option">Categoría 1</h5>
                               <a href="#" className="list-group-item item-sm">
-                                <span className="material-symbols-rounded o-icon" aria-hidden="true">info</span>
+                                <span className="material-symbols-rounded o-icon" aria-hidden="true">
+                                  info
+                                </span>
                                 <div className="access-content">
                                   <span className="access-title">Acceso chico</span>
                                 </div>
                               </a>
                               <a href="#" className="list-group-item item-sm">
-                                <span className="material-symbols-rounded o-icon" aria-hidden="true">info</span>
+                                <span className="material-symbols-rounded o-icon" aria-hidden="true">
+                                  info
+                                </span>
                                 <div className="access-content">
                                   <span className="access-title">Acceso chico</span>
                                 </div>
@@ -184,7 +196,9 @@ const AccessOrganismDocs: React.FC = () => {
                           </div>
                           <hr className="clearfix" />
                           <a href="#" className="list-group-item card-item">
-                            <span className="material-symbols-rounded o-icon" aria-hidden="true">info</span>
+                            <span className="material-symbols-rounded o-icon" aria-hidden="true">
+                              info
+                            </span>
                             <div className="access-content">
                               <span className="access-title">Acceso grande</span>
                             </div>

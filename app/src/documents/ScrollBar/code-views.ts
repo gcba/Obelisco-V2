9183fb95748d1180c1fbbe03fc10f6812c3466e1
Victoria Nastasi <victorianastasi@gmail.com>
2024-10-14 12:38:28 -0300

    fix: ajuste de estilos

--- app/src/documents/ScrollBar/code-views.ts
+++ app/src/documents/ScrollBar/code-views.ts
@@ -1,47 +1,46 @@
-export const SCROLLBAR_HORIZONTAL = `<div class="responsive-scroll" tabindex="0">
+export const SCROLLBAR_HORIZONTAL = `
+<div class="responsive-scroll" tabindex="0">
+  <div class="horizontal-example">
     <h1>Esto es un título de ejemplo</h1>
-    <div class="horizontal-example">
-      Esto es un párrafo de ejemplo para probar el funcionamiento de la barra
-      <p class="lead">
-        muestra. El ancho del texto es para probar el desplazamiento horizontal,
-        de desplazamiento. No tiene ningún fin específico, solo sirve de
-        probar el desplazamiento vertical.
-        mientras que la lista de cuerpos de contenido consecutivos es para
-      <br />
-      </p>
-      <br />
-      <hr />
-      <p>
-        <h2>Esto es un título de ejemplo</h2>
-        de desplazamiento. No tiene ningún fin específico, solo sirve de
-        Esto es un párrafo de ejemplo para probar el funcionamiento de la barra
-        mientras que la lista de cuerpos de contenido consecutivos es para
-        muestra. El ancho del texto es para probar el desplazamiento horizontal,
-      </p>
-      probar el desplazamiento vertical.
-    </div>
+    <p class="lead">
+      Esto es un párrafo de ejemplo para probar el funcionamiento de la barra de desplazamiento. No tiene ningún fin específico, solo sirve de muestra. El ancho del texto es para probar el desplazamiento horizontal, mientras que la lista de cuerpos de contenido consecutivos es para probar el desplazamiento vertical.
+    </p>
+    <br />
+    <hr />
+    <br />
+    <h2>Esto es un título de ejemplo</h2>
+    <p>
+      Esto es un párrafo de ejemplo para probar el funcionamiento de la barra de desplazamiento. No tiene ningún fin específico, solo sirve de muestra. El ancho del texto es para probar el desplazamiento horizontal, mientras que la lista de cuerpos de contenido consecutivos es para probar el desplazamiento vertical.
+    </p>
+  </div>
 </div>`;
 
-export const SCROLLBAR_VERTICAL = `<div class="responsive-scroll" tabindex="0">
+export const SCROLLBAR_VERTICAL = `
+<div class="responsive-scroll" tabindex="0">
+  <div class="vertical-example">
     <h1>Esto es un título de ejemplo</h1>
-    <div class="vertical-example">
-      Esto es un párrafo de ejemplo para probar el funcionamiento de la barra
-      <p class="lead">
-        muestra. El ancho del texto es para probar el desplazamiento horizontal,
-        de desplazamiento. No tiene ningún fin específico, solo sirve de
-        probar el desplazamiento vertical.
-        mientras que la lista de cuerpos de contenido consecutivos es para
-      <br />
-      </p>
-      <br />
-      <hr />
-      <p>
-        <h2>Esto es un título de ejemplo</h2>
-        de desplazamiento. No tiene ningún fin específico, solo sirve de
-        Esto es un párrafo de ejemplo para probar el funcionamiento de la barra
-        mientras que la lista de cuerpos de contenido consecutivos es para
-        muestra. El ancho del texto es para probar el desplazamiento horizontal,
-      </p>
-      probar el desplazamiento vertical.
-    </div>
+    <p class="lead">
+      Esto es un párrafo de ejemplo para probar el funcionamiento de la barra de desplazamiento. No tiene ningún fin específico, solo sirve de muestra. El ancho del texto es para probar el desplazamiento horizontal, mientras que la lista de cuerpos de contenido consecutivos es para probar el desplazamiento vertical.
+    </p>
+    <br />
+    <hr />
+    <br />
+    <p>
+      Esto es un párrafo de ejemplo para probar el funcionamiento de la barra de desplazamiento. No tiene ningún fin específico, solo sirve de muestra. El ancho del texto es para probar el desplazamiento horizontal, mientras que la lista de cuerpos de contenido consecutivos es para probar el desplazamiento vertical.
+    </p>
+    <br />
+    <hr />
+    <br />
+    <h2>Esto es un título de ejemplo</h2>
+    <p>
+      Esto es un párrafo de ejemplo para probar el funcionamiento de la barra de desplazamiento. No tiene ningún fin específico, solo sirve de muestra. El ancho del texto es para probar el desplazamiento horizontal, mientras que la lista de cuerpos de contenido consecutivos es para probar el desplazamiento vertical.
+    </p>
+    <br />
+    <hr />
+    <br />
+    <h2>Esto es un título de ejemplo</h2>
+    <p>
+      Esto es un párrafo de ejemplo para probar el funcionamiento de la barra de desplazamiento. No tiene ningún fin específico, solo sirve de muestra. El ancho del texto es para probar el desplazamiento horizontal, mientras que la lista de cuerpos de contenido consecutivos es para probar el desplazamiento vertical.
+    </p>
+  </div>
 </div>`;

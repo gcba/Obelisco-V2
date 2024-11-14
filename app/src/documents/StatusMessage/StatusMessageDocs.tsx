17c9ffc91df1a4db0d64ab92be4d442f736b05b4
Victoria Nastasi <victorianastasi@gmail.com>
2024-11-14 11:58:19 -0300

    fix: ajustes en mensaje de estado

--- app/src/documents/StatusMessage/StatusMessageDocs.tsx
+++ app/src/documents/StatusMessage/StatusMessageDocs.tsx
@@ -31,19 +31,29 @@ const sections = [
     title: 'Información',
     content: (
       <CodeBox codeHTML={SM_INFO}>
-        <div className="px-4 col-sm-12 col-md-8">
+        <div className="px-3">
           <div className="status status-info">
-            <h2 className="status-title">Esto es un título de un mensaje de información</h2>
+            <h2 className="status-title">Este es el título de un mensaje de estado</h2>
             <p className="status-text">
-              Este es un párrafo que brinda información necesaria con la finalidad de guiar y ayudar a las personas
-              usuarias a finalizar un proceso o iniciar uno nuevo.
+              Este es un párrafo breve que sirve como mensaje. Puede utilizarse para dar indicaciones adicionales como la realización de una tarea o instrucciones a seguir.
             </p>
-            <p className="status-text">
-              Este es un párrafo que brinda información necesaria con la finalidad de guiar y ayudar a las personas
-              usuarias a finalizar un proceso o iniciar uno nuevo.
-            </p>
+            <div className="list-informative status-list">
+              <ul className="list-informative-bullet">
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+              </ul>
+            </div>
             <button type="button" className="btn btn-primary">
-              <i className="bx bxs-info-circle o-icon"></i>
               Botón
             </button>
             <button type="button" className="btn btn-outline-primary">
@@ -59,22 +69,29 @@ const sections = [
     title: 'Éxito',
     content: (
       <CodeBox codeHTML={SM_SUCCESS}>
-        <div className="px-4 col-sm-12 col-md-8">
+        <div className="px-3">
           <div className="status status-success">
-            <h2 className="status-title">Esto es un título de un mensaje de éxito</h2>
+            <h2 className="status-title">Este es el título de un mensaje de estado</h2>
             <p className="status-text">
-              Esto es un párrafo que acompaña y sirve para indicar que el usuario realizó una acción correctamente, como
-              cuando realiza una tarea con éxito.
+              Este es un párrafo breve que sirve como mensaje. Puede utilizarse para dar indicaciones adicionales como la realización de una tarea o instrucciones a seguir.
             </p>
-            <ul className="status-list">
-              <li>
-                Éste es un ítem dentro de una lista que puede contener un <LinkClient href="#">enlace</LinkClient>.
-              </li>
-              <li>Los ítems pueden contener o no enlaces. Este es un ejemplo para un detalle que no lleva enlace.</li>
-              <li>Éste es otro ítem dentro de una lista.</li>
-            </ul>
+            <div className="list-informative status-list">
+              <ul className="list-informative-bullet">
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+              </ul>
+            </div>
             <button type="button" className="btn btn-primary">
-              <i className="bx bxs-info-circle o-icon"></i>
               Botón
             </button>
             <button type="button" className="btn btn-outline-primary">
@@ -90,22 +107,29 @@ const sections = [
     title: 'Advertencia',
     content: (
       <CodeBox codeHTML={SM_WARNING}>
-        <div className="px-4 col-sm-12 col-md-8">
+        <div className="px-3">
           <div className="status status-warning">
-            <h2 className="status-title">Esto es un título de un mensaje de advertencia</h2>
+            <h2 className="status-title">Este es el título de un mensaje de estado</h2>
             <p className="status-text">
-              Esto es un párrafo que advierte y sirve para indicarle al usuario que preste atención a determinada
-              información o acción que debe realizar.
+              Este es un párrafo breve que sirve como mensaje. Puede utilizarse para dar indicaciones adicionales como la realización de una tarea o instrucciones a seguir.
             </p>
-            <ul className="status-list">
-              <li>
-                Contar con Clave Ciudad de <LinkClient href="#">AGIP</LinkClient>
-              </li>
-              <li>Cargar en el Sistema de Tramitación a Distancia (TAD) la siguiente documentación obligatoria:</li>
-              <li>Anexo II Formulario de Presentación de Programa de Capacitación</li>
-            </ul>
+            <div className="list-informative status-list">
+              <ul className="list-informative-bullet">
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+              </ul>
+            </div>
             <button type="button" className="btn btn-primary">
-              <i className="bx bxs-info-circle o-icon"></i>
               Botón
             </button>
             <button type="button" className="btn btn-outline-primary">
@@ -121,22 +145,29 @@ const sections = [
     title: 'Error',
     content: (
       <CodeBox codeHTML={SM_DANGER}>
-        <div className="px-4 col-sm-12 col-md-8">
+        <div className="px-3">
           <div className="status status-danger">
-            <h2 className="status-title">Esto es un título de un mensaje de peligro</h2>
+            <h2 className="status-title">Este es el título de un mensaje de estado</h2>
             <p className="status-text">
-              Este párrafo sirve para comunicar que algo salió mal y explica cómo puede ser solucionado. Muestra errores
-              sobre algo que se realizó.
+              Este es un párrafo breve que sirve como mensaje. Puede utilizarse para dar indicaciones adicionales como la realización de una tarea o instrucciones a seguir.
             </p>
-            <ul className="status-list">
-              <li>
-                Contar con Clave Ciudad de <LinkClient href="#">AGIP</LinkClient>
-              </li>
-              <li>Cargar en el Sistema de Tramitación a Distancia (TAD) la siguiente documentación obligatoria:</li>
-              <li>Anexo II Formulario de Presentación de Programa de Capacitación</li>
-            </ul>
+            <div className="list-informative status-list">
+              <ul className="list-informative-bullet">
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+                <li>
+                  Este es el contenido de un ítem de una lista general. Puede contener un <LinkClient>enlace</LinkClient>.
+                </li>
+              </ul>
+            </div>
             <button type="button" className="btn btn-primary">
-              <i className="bx bxs-info-circle o-icon"></i>
               Botón
             </button>
             <button type="button" className="btn btn-outline-primary">
@@ -152,15 +183,13 @@ const sections = [
     title: 'Institucional',
     content: (
       <CodeBox codeHTML={SM_INSTITUTIONAL}>
-        <div className="px-4 col-sm-12 col-md-8">
+        <div className="px-3">
           <div className="status status-institutional">
-            <h2 className="status-title">Este es un mensaje institucional del Gobierno de la Ciudad de Buenos Aires</h2>
+            <h2 className="status-title">Este es el título de un mensaje de estado</h2>
             <p className="status-text">
-              En el cuerpo de este componente pueden ir enlaces o botones:
-              <LinkClient href="#">https://ejemplo.buenosaires.gob.ar</LinkClient>
+              Este es un párrafo breve que sirve como mensaje. Puede utilizarse para dar indicaciones adicionales como la realización de una tarea o instrucciones a seguir. Puede contener un <LinkClient>enlace</LinkClient>.
             </p>
             <button type="button" className="btn btn-primary">
-              <i className="bx bxs-info-circle o-icon"></i>
               Botón
             </button>
             <button type="button" className="btn btn-outline-primary">

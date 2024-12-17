a322e776c27ebd746f8a1116094b95dc45668388
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-12-17 11:14:05 -0300

    docs: Tab de Guía de Uso (Header)
    Falta: Especificaciones y Accesibilidad. Error de tipos en linea 2332

--- app/src/documents/Header/HeaderDocs.tsx
+++ app/src/documents/Header/HeaderDocs.tsx
@@ -2118,6 +2118,196 @@ const HeaderDocs: React.FC = () => {
     },
   ];
 
+  const GUIDE_OF_USE = [
+    {
+      title: 'Uso',
+    },
+    {
+      subtitle: 'Cuándo usar',
+      content: (
+        <>
+          <div className="container">
+            <p className='text-xl' ></p>
+            <ul className='list-informative' >
+              <li>Debe estar presente en todas las páginas de forma consistente, para facilitar la navegación de la persona usuaria a través del sitio web.</li>
+              <li>Para mantener las navegaciones principales y la presencia de la marca en un primer nivel.</li>
+            </ul>
+          </div>
+        </>
+      ),
+    },
+    {
+      subtitle: 'Cuándo no usar',
+      content: (
+        <>
+          <div className="container">
+            <p className='text-xl' ></p>
+            <ul className='list-informative' >
+              <li>Para categorías internas o filtrado dentro de una página, utilizar <a href="https://gcba.github.io/Obelisco-V2/components/tabs">Pestañas</a> en su lugar.</li>
+              <li>Para mantener las navegaciones principales y la presencia de la marca en un primer nivel.</li>
+              <li>
+                <strong>
+                  Para navegaciones de 2do nivel o subsecciones de contenido, considerar la </strong> <a href="https://gcba.github.io/Obelisco-V2/components/nav-horizontal">Navegación horizontal</a> <strong>o la </strong><a href="https://gcba.github.io/Obelisco-V2/components/nav-vertical">Navegación vertical</a> <strong>dependiendo del tipo de página</strong>.
+              </li>
+            </ul>
+          </div>
+        </>
+      ),
+    },
+    {
+      title: 'Ejemplos de uso',
+    },
+    {
+      subtitle: 'Navegación principal',
+      content: (
+        <>
+          <p className='text-md' >Los enlaces de navegación redirigen a las secciones y/o páginas principales de la arquitectura de información del sitio web.</p>
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <img
+                  src="/Obelisco-V2/images/header/header_Columnas_si_usar.svg"
+                  alt="Cómo usar el header"
+                  className="img-fluid" />
+                <p className="text-sm">
+                  ✅ Utilizar una opción de navegación con link de acceso cuando hay más de 6 opciones.
+                </p>
+
+              </div>
+              <div className="col-6">
+                <img
+                  src="/Obelisco-V2/images/header/header_Columnas_no_usar.svg"
+                  alt="Cómo no usar el header"
+                  className="img-fluid" />
+                <p className="text-sm">
+                  ❌ No utilizar una opción link si las subsecciones no superan las 6 opciones.
+                </p>
+              </div>
+            </div>
+          </div>
+        </>
+      ),
+    },
+    {
+      subtitle: 'Opciones de navegación',
+      content: (
+        <>
+          <p className="text-md">
+            Las opciones de navegación se utilizan como subsecciones dentro de un desplegable de navegación del encabezado <i>(header)</i>.
+          </p>
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <img
+                  src="/Obelisco-V2/images/header/header_opciones_de_navegacion_si.svg"
+                  alt="Cómo usar el header"
+                  className="img-fluid" />
+                <p className="text-sm">
+                  ✅Utilizar hasta 7 enlaces de navegación, dependiendo del tipo de encabezado <i>(header)</i>.
+                </p>
+
+              </div>
+              <div className="col-6">
+                <img
+                  src="/Obelisco-V2/images/header/header_opciones_de_navegacion_no.svg"
+                  alt="Cómo no usar el header"
+                  className="img-fluid" />
+                <p className="text-sm">
+                  ❌ No utilizar más de 7 enlaces de navegación, o más de las navegaciones disponibles para cada tipo de encabezado <i>(header)</i> y sus variantes.
+                </p>
+              </div>
+            </div>
+          </div>
+        </>
+      ),
+    },
+    {
+      title: 'Tipos de encabezado (header)',
+      content: (
+        <>
+          <p className="text-md">
+            Existen 2 tipos de header según la cantidad de navegaciones y el estado de la cuenta de la persona usuaria (con sesión iniciada y sin iniciar sesión).
+          </p>
+
+
+        </>
+      ),
+    },
+    {
+      subtitle: 'Desktop deslogueado | En 1 línea',
+      content: (
+        <>
+          <p className="text-md mt-2 mb-4">El encabezado <i>(header)</i> de 1 línea es el tipo de encabezado que se utiliza por defecto.</p>
+          <div className="container">
+            <img
+              src="/Obelisco-V2/images/header/header_desktop_deslogueado_1_linea.svg"
+              alt="Cómo no usar el header"
+              className="img-fluid" />
+          </div>
+        </>
+      ),
+    },
+    {
+      subtitle: 'Desktop logueado | En 1 línea',
+      content: (
+        <>
+          <p className="text-md mt-2 mb-4">El encabezado <i>(header)</i> de 1 línea es el tipo de encabezado que se utiliza por defecto.</p>
+          <div className="container">
+            <img
+              src="/Obelisco-V2/images/header/header_desktop_logueado_2_lineas.svg"
+              alt="Cómo no usar el header"
+              className="img-fluid" />
+          </div>
+
+        </>
+      ),
+    },
+    {
+      subtitle: 'Desktop deslogueado | En 2 líneas',
+      content: (
+        <>
+          <p className="text-md mt-2 mb-4">El encabezado <i>(header)</i> de 2 líneas se utiliza cuando la cantidad de navegaciones excede al espacio disponible en 1 línea.</p>
+          <div className="container">
+            <img
+              src="/Obelisco-V2/images/header/header_desktop_logueado_2_lineas.svg"
+              alt="Cómo no usar el header"
+              className="img-fluid" />
+          </div>
+        </>
+      ),
+    },
+    ,
+    {
+      subtitle: 'Desktop logueado | En 2 líneas',
+      content: (
+        <>
+          <p className="text-md mt-2 mb-4">El encabezado <i>(header)</i> de 2 líneas se utiliza cuando la cantidad de navegaciones excede al espacio disponible en 1 línea.</p>
+          <div className="container">
+            <img
+              src="/Obelisco-V2/images/header/header_desktop_logueado_2_lineas.svg"
+              alt="Cómo no usar el header"
+              className="img-fluid" />
+          </div>
+        </>
+      ),
+    }
+    ,
+    {
+      subtitle: 'Mobile',
+      content: (
+        <>
+          <p className="text-md mt-2 mb-4">El encabezado <i>(header)</i> en su versión mobile pasa a ser un menú desplegable con la reorganización de los elementos que lo componen.</p>
+          <div className="container">
+            <img
+              src="/Obelisco-V2/images/header/header_mobile"
+              alt="Cómo no usar el header"
+              className="img-fluid" />
+          </div>
+        </>
+      ),
+    }
+  ];
+
   return (
     <>
       <ComponentHeader
@@ -2138,7 +2328,11 @@ const HeaderDocs: React.FC = () => {
           </>,
         ]}
       />
-      <Tabs sectionDev={SECTIONS_DEV} />
+      <Tabs
+        sectionUx={GUIDE_OF_USE}
+        sectionDev={SECTIONS_DEV}
+      />
+
     </>
   );
 };

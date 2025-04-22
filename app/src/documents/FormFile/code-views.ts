6ba9f0e52d963430bdc4de941ce846e95ab4285c
Mathias Barbosa Velazquez <mathibar03@gmail.com>
2025-04-22 11:34:03 -0300

    fix: se elimino div con clase form-wrapper de los code-views

--- app/src/documents/FormFile/code-views.ts
+++ app/src/documents/FormFile/code-views.ts
@@ -1,5 +1,4 @@
-export const FILE_DEFAULT = `<div class="form-wrapper">
-    <div class="form-label-badge">
+export const FILE_DEFAULT = `<div class="form-label-badge">
       <label for="exampleInputFile" class="form-label">Label</label>
       <span class="badge-forms badge-required-forms">Requerido</span>
     </div>
@@ -7,11 +6,9 @@ export const FILE_DEFAULT = `<div class="form-wrapper">
       Este es un texto de ayuda que da mas información sobre el Input. Es opcional, pero recomendado, y de no más de 3 líneas.
     </p>
     <input type="file" class="form-control" id="exampleInputFile" accept="image/png, image/jpeg" />
-  </div>
 `;
 
-export const FILE_DISABLED = `<div class="form-wrapper">
-    <div class="form-label-badge">
+export const FILE_DISABLED = `<div class="form-label-badge">
       <label for="exampleInputFile" class="form-label">Label</label>
       <span class="badge-forms badge-required-forms">Requerido</span>
     </div>
@@ -19,5 +16,5 @@ export const FILE_DISABLED = `<div class="form-wrapper">
       Este es un texto de ayuda que da mas información sobre el Input. Es opcional, pero recomendado, y de no más de 3 líneas.
     </p>
     <input type="file" class="form-control" id="exampleInputFile" accept="image/png, image/jpeg" disabled />
-  </div>
+  
 `;
--- app/src/documents/FormValidation/code-views.ts
+++ app/src/documents/FormValidation/code-views.ts
@@ -1,5 +1,4 @@
-export const INPUT_VALIDATION_TEXT = `<div class="form-wrapper">
-    <div class="form-label-badge">
+export const INPUT_VALIDATION_TEXT = `<div class="form-label-badge">
       <label for="exampleInputTexto" class="form-label">Label</label>
       <span class="badge-forms badge-required-forms">Requerido</span>
     </div>
@@ -8,11 +7,9 @@ export const INPUT_VALIDATION_TEXT = `<div class="form-wrapper">
     </p>
     <input type="text" class="form-control is-invalid" id="exampleInputTexto" aria-describedby="invalid-feedback-texto" placeholder="Texto" />
     <p id="invalid-feedback-texto" class="invalid-feedback">Los datos introducidos son inválidos.</p>
-  </div>
 `;
 
-export const INPUT_VALIDATION_NUMBER = `<div class="form-wrapper">
-    <div class="form-label-badge">
+export const INPUT_VALIDATION_NUMBER = `<div class="form-label-badge">
       <label for="exampleInputNumber" class="form-label">Label</label>
       <span class="badge-forms badge-required-forms">Requerido</span>
     </div>
@@ -21,11 +18,9 @@ export const INPUT_VALIDATION_NUMBER = `<div class="form-wrapper">
     </p>
     <input type="text" class="form-control is-valid" id="exampleInputNumber" aria-describedby="valid-feedback-texto" placeholder="Texto" />
     <p id="valid-feedback-texto" class="valid-feedback">Los datos introducidos son válidos.</p>
-  </div>
 `;
 
-export const INPUT_VALIDATION_MAIL = `<div class="form-wrapper">
-    <div class="form-label-badge">
+export const INPUT_VALIDATION_MAIL = `<div class="form-label-badge">
       <label for="exampleInputText" class="form-label">Label</label>
       <span class="badge-forms badge-required-forms">Requerido</span>
     </div>
@@ -34,11 +29,9 @@ export const INPUT_VALIDATION_MAIL = `<div class="form-wrapper">
     </p>
     <input type="email" class="form-control is-invalid" id="ejemplo-3-1" aria-describedby="Email" placeholder="Email@test.com" required />
     <p id="invalid-feedback-texto" class="invalid-feedback">Los datos introducidos son inválidos.</p>
-  </div>
 `;
 
-export const INPUT_VALIDATION_DATE = `<div class="form-wrapper">
-    <div class="form-label-badge">
+export const INPUT_VALIDATION_DATE = `<div class="form-label-badge">
       <label for="ejemplo-4" class="form-label">Ejemplo campo de fecha</label>
       <span class="badge-forms badge-required-forms">Requerido</span>
     </div>
@@ -47,11 +40,9 @@ export const INPUT_VALIDATION_DATE = `<div class="form-wrapper">
     </p>
     <input type="date" class="form-control is-valid" id="ejemplo-4" aria-describedby="Date" placeholder="selecciona tu fecha" required />
     <p class="valid-feedback">El campo es válido.</p>
-  </div>
 `;
 
-export const INPUT_VALIDATION_TEXTAREA = `<div class="form-wrapper">
-    <div class="form-label-badge">
+export const INPUT_VALIDATION_TEXTAREA = `<div class="form-label-badge">
       <label for="ejemplo-5" class="form-label">Ejemplo TextArea</label>
       <span class="badge-forms badge-required-forms">Requerido</span>
     </div>
@@ -60,5 +51,4 @@ export const INPUT_VALIDATION_TEXTAREA = `<div class="form-wrapper">
     </p>
     <textarea class="form-control is-invalid" id="ejemplo-5" required></textarea>
     <p class="invalid-feedback">El campo es inválido</p>
-  </div>
 `;

d8325ef6fa5411fd217834ca251c2e655a128bfc
Victoria Rodriguez <99053409+VictoriaIleanaRodriguezMora@users.noreply.github.com>
2025-07-07 15:54:16 -0300

    feat: disposicion con scroll, en rama galeria (#187)
    * feat: Disposición con scroll para Tarjetas verticales de Noticias
    
    * docs: Lint
    
    * fix: Codebox y duplicado en sass

--- app/src/documents/Card/CardDocs.tsx
+++ app/src/documents/Card/CardDocs.tsx
@@ -15,6 +15,7 @@ import {
   HORIZONTAL_IMG,
   HORIZONTAL_TEXT,
   HORIZONTAL_THEME,
+  SCROLL_DISPOSITION,
   TARJETAS_ACCESSIBILTY,
   TARJETAS_ACCESSIBILTY_2,
   VERTICAL_AGENDA,
@@ -334,6 +335,171 @@ const CardDocs: React.FC = () => {
     },
     {
       title: 'Disposición',
+    },
+    {
+      subtitle: 'Disposicion con scroll',
+      content: (
+        <CodeBox codeHTML={SCROLL_DISPOSITION}>
+          <div className="card-box-sizing-disp-scroll">
+            <div className="container">
+              <div className="responsive-scroll has-card-items" tabIndex={0}>
+                <div className="card-items-3">
+                  <div className="col">
+                    <div className="card">
+                      <Image
+                        src={`${basePath}/images/card-img-top.jpg`}
+                        className="card-img"
+                        alt="descripción de imagen"
+                        width={348}
+                        height={196}
+                      ></Image>{' '}
+                      <div className="card-body">
+                        <div className="card-badges">
+                          <span className="badge badge-default">etiqueta 1</span>
+                          <span className="badge badge-default">etiqueta 2</span>
+                        </div>
+                        <h3 className="card-title">
+                          <a href="#" className="card-title-link">
+                            Título de la tarjeta
+                          </a>
+                        </h3>
+                        <p className="card-text">Descripción de la tarjeta</p>
+                        <div className="card-info">
+                          <div>
+                            <small>
+                              <span>Fecha de publicación</span>
+                            </small>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col">
+                    <div className="card">
+                      <Image
+                        src={`${basePath}/images/card-img-top.jpg`}
+                        className="card-img"
+                        alt="descripción de imagen"
+                        width={348}
+                        height={196}
+                      ></Image>
+                      <div className="card-body">
+                        <div className="card-badges">
+                          <span className="badge badge-default">etiqueta 1</span>
+                          <span className="badge badge-default">etiqueta 2</span>
+                        </div>
+                        <h3 className="card-title">
+                          <a href="#" className="card-title-link">
+                            Título de la tarjeta
+                          </a>
+                        </h3>
+                        <p className="card-text">Descripción de la tarjeta</p>
+                        <div className="card-info">
+                          <div>
+                            <small>
+                              <span>Fecha de publicación</span>
+                            </small>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col">
+                    <div className="card">
+                      <Image
+                        src={`${basePath}/images/card-img-top.jpg`}
+                        className="card-img"
+                        alt="descripción de imagen"
+                        width={348}
+                        height={196}
+                      ></Image>
+                      <div className="card-body">
+                        <div className="card-badges">
+                          <span className="badge badge-default">etiqueta 1</span>
+                          <span className="badge badge-default">etiqueta 2</span>
+                        </div>
+                        <h3 className="card-title">
+                          <a href="#" className="card-title-link">
+                            Título de la tarjeta
+                          </a>
+                        </h3>
+                        <p className="card-text">Descripción de la tarjeta</p>
+                        <div className="card-info">
+                          <div>
+                            <small>
+                              <span>Fecha de publicación</span>
+                            </small>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col">
+                    <div className="card">
+                      <Image
+                        src={`${basePath}/images/card-img-top.jpg`}
+                        className="card-img"
+                        alt="descripción de imagen"
+                        width={348}
+                        height={196}
+                      ></Image>
+                      <div className="card-body">
+                        <div className="card-badges">
+                          <span className="badge badge-default">etiqueta 1</span>
+                          <span className="badge badge-default">etiqueta 2</span>
+                        </div>
+                        <h3 className="card-title">
+                          <a href="#" className="card-title-link">
+                            Título de la tarjeta
+                          </a>
+                        </h3>
+                        <p className="card-text">Descripción de la tarjeta</p>
+                        <div className="card-info">
+                          <div>
+                            <small>
+                              <span>Fecha de publicación</span>
+                            </small>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* <div className="col">
+                    <div className="card">
+                      <Image
+                    src={`${basePath}/images/card-img-top.jpg`}
+                    className="card-img"
+                    alt="descripción de imagen"
+                    width={348}
+                    height={196}
+                  ></Image>
+                      <div className="card-body">
+                        <div className="card-badges">
+                          <span className="badge badge-default">etiqueta 1</span>
+                          <span className="badge badge-default">etiqueta 2</span>
+                        </div>
+                        <h3 className="card-title">
+                          <a href="#" className="card-title-link">
+                            Título de la tarjeta
+                          </a>
+                        </h3>
+                        <p className="card-text">Descripción de la tarjeta</p>
+                        <div className="card-info">
+                          <div>
+                            <small>
+                              <span>Fecha de publicación</span>
+                            </small>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </CodeBox>
+      ),
     },
     {
       subtitle: 'Tres columnas',

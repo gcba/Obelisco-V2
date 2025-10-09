0cf2417a8f76623d067466729ed0d52cd9363198
Alex <alexander.pantyukhov@w3americas.com>
2025-10-09 12:11:01 -0300

    feat: carousel component

--- app/src/documents/HeroTemplate/HeroTemplate.tsx
+++ app/src/documents/HeroTemplate/HeroTemplate.tsx
@@ -7,6 +7,47 @@ const basePath = '/Obelisco-V2';
 
 const sections = [
   {
+    title: 'carousel',
+    content: (
+      <div className="container">
+        <div id="carousel" className="carousel slide" data-bs-ride="carousel">
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carousel"
+              data-bs-slide-to="0"
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+            <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src="https://picsum.photos/800/600" className="d-block w-100" alt="Slide 1" />
+            </div>
+            <div className="carousel-item">
+              <img src="https://picsum.photos/800/600" className="d-block w-100" alt="Slide 2" />
+            </div>
+            <div className="carousel-item">
+              <img src="https://picsum.photos/800/600" className="d-block w-100" alt="Slide 3" />
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    ),
+  },
+  {
     title: '4/4 Input Buscador ',
     content: (
       <div className="col-12">

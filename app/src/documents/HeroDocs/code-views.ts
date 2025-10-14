0ccf7ae6bee8e9b462ed8ec9899257000beb35fe
Alex <alexander.pantyukhov@w3americas.com>
2025-10-14 11:56:50 -0300

    fix: content responsive padding

--- app/src/documents/HeroDocs/code-views.ts
+++ app/src/documents/HeroDocs/code-views.ts
@@ -1,6 +1,6 @@
 export const HERO_INSTITUCIONAL = `<header class="hero">
   <div class="content corner">
-    <div class="d-flex flex-column py-3 py-lg-5">
+    <div class="d-flex flex-column">
       <nav aria-label="Navegación secundaria">
         <ol class="breadcrumb m-0">
           <li class="breadcrumb-item">
@@ -30,7 +30,7 @@ export const HERO_INSTITUCIONAL = `<header class="hero">
 
 export const HERO_DINAMICO = `<header class="hero">
    <div class="content wave">
-      <div class="d-flex flex-column py-3 py-lg-5">
+      <div class="d-flex flex-column">
          <nav aria-label="Navegación secundaria">
             <ol class="breadcrumb m-0">
                <li class="breadcrumb-item"><a href="#">Inicio</a></li>
@@ -61,7 +61,7 @@ export const HERO_DINAMICO = `<header class="hero">
 
 export const HERO_CON_IMAGEN = `<header class="hero">
    <div class="content wave">
-      <div class="d-flex flex-column py-3 py-lg-5">
+      <div class="d-flex flex-column">
          <nav aria-label="Navegación secundaria">
             <ol class="breadcrumb m-0">
                <li class="breadcrumb-item"><a href="#">Inicio</a></li>
@@ -86,7 +86,7 @@ export const HERO_CON_IMAGEN = `<header class="hero">
 
 export const HERO_CON_VIDEO = `<header class="hero">
    <div class="content wave">
-      <div class="d-flex flex-column py-3 py-lg-5">
+      <div class="d-flex flex-column">
          <nav aria-label="Navegación secundaria">
             <ol class="breadcrumb m-0">
                <li class="breadcrumb-item"><a href="#">Inicio</a></li>
@@ -115,7 +115,7 @@ export const HERO_CON_VIDEO = `<header class="hero">
 
 export const HERO_CON_GIF = `<header class="hero">
    <div class="content wave">
-      <div class="d-flex flex-column py-3 py-lg-5">
+      <div class="d-flex flex-column">
          <nav aria-label="Navegación secundaria">
             <ol class="breadcrumb m-0">
                <li class="breadcrumb-item"><a href="#">Inicio</a></li>
@@ -144,7 +144,7 @@ export const HERO_CON_GIF = `<header class="hero">
 
 export const HERO_CON_CAMPO_BUSCADOR = `<header class="hero">
   <div class="content corner">
-    <div class="d-flex flex-column py-3 py-lg-5">
+    <div class="d-flex flex-column">
       <nav aria-label="Navegación secundaria">
         <ol class="breadcrumb m-0">
           <li class="breadcrumb-item">
@@ -159,41 +159,43 @@ export const HERO_CON_CAMPO_BUSCADOR = `<header class="hero">
           convivencia armónica y responsable de las mascotas y sus responsables en el espacio público.
         </p>
       </div>
-      <div class="search-wrapper">
-        <label htmlFor="search-home" class="sr-only">Buscar</label>
-        <input 
-          type="text" 
-          id="search-home" 
-          maxlength="100"
-          autocomplete="off" 
-          class="form-control" 
-          placeholder="¿En qué te podemos ayudar?" 
-          name="search-home" 
-        />
-        <div class="search-btn-container">
-           <button class="btn btn-lg btn-primary btn-search">
-              Buscar
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                 <path d="M11.3137 10.0598H10.6553L10.422 9.83477C11.422 8.6681 11.9387 7.07643 11.6553 5.38477C11.2637 3.0681 9.33034 1.2181 6.99701 0.934766C3.47201 0.501433 0.505339 3.4681 0.938672 6.9931C1.22201 9.32643 3.07201 11.2598 5.38867 11.6514C7.08034 11.9348 8.67201 11.4181 9.83867 10.4181L10.0637 10.6514V11.3098L13.6053 14.8514C13.947 15.1931 14.5053 15.1931 14.847 14.8514C15.1887 14.5098 15.1887 13.9514 14.847 13.6098L11.3137 10.0598ZM6.31367 10.0598C4.23867 10.0598 2.56367 8.38477 2.56367 6.30977C2.56367 4.23477 4.23867 2.55977 6.31367 2.55977C8.38867 2.55977 10.0637 4.23477 10.0637 6.30977C10.0637 8.38477 8.38867 10.0598 6.31367 10.0598Z" fill="white">
-                 </path>
-              </svg>
-           </button>
-        </div>
-        <div class="search-results bg-light">
-           <div>
-              <a href="#" target="_blank" rel="noopener noreferrer" 
-              class="list-link-result">
-                <span class="material-symbols-outlined">search</span>
-                Enlace predeterminado
-              </a>
-              <a href="#" target="_blank" rel="noopener noreferrer" 
-              class="list-link-result">
-                <span class="material-symbols-outlined">search</span>
-                Enlace predeterminado
-              </a>
-            </div>
-        </div>
-      </div>
+			<div class="search-container>
+				<div class="search-wrapper">
+					<label htmlFor="search-home" class="sr-only">Buscar</label>
+					<input 
+						type="text" 
+						id="search-home" 
+						maxlength="100"
+						autocomplete="off" 
+						class="form-control" 
+						placeholder="¿En qué te podemos ayudar?" 
+						name="search-home" 
+					/>
+					<div class="search-btn-container">
+						<button class="btn btn-lg btn-primary btn-search">
+								Buscar
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+									<path d="M11.3137 10.0598H10.6553L10.422 9.83477C11.422 8.6681 11.9387 7.07643 11.6553 5.38477C11.2637 3.0681 9.33034 1.2181 6.99701 0.934766C3.47201 0.501433 0.505339 3.4681 0.938672 6.9931C1.22201 9.32643 3.07201 11.2598 5.38867 11.6514C7.08034 11.9348 8.67201 11.4181 9.83867 10.4181L10.0637 10.6514V11.3098L13.6053 14.8514C13.947 15.1931 14.5053 15.1931 14.847 14.8514C15.1887 14.5098 15.1887 13.9514 14.847 13.6098L11.3137 10.0598ZM6.31367 10.0598C4.23867 10.0598 2.56367 8.38477 2.56367 6.30977C2.56367 4.23477 4.23867 2.55977 6.31367 2.55977C8.38867 2.55977 10.0637 4.23477 10.0637 6.30977C10.0637 8.38477 8.38867 10.0598 6.31367 10.0598Z" fill="white">
+									</path>
+								</svg>
+						</button>
+					</div>
+					<div class="search-results bg-light">
+						<div>
+								<a href="#" target="_blank" rel="noopener noreferrer" 
+								class="list-link-result">
+									<span class="material-symbols-outlined">search</span>
+									Enlace predeterminado
+								</a>
+								<a href="#" target="_blank" rel="noopener noreferrer" 
+								class="list-link-result">
+									<span class="material-symbols-outlined">search</span>
+									Enlace predeterminado
+								</a>
+							</div>
+					</div>
+				</div>
+			</div>
     </div>
   </div>
   <div class="aside bg-aside-hero-dark"></div>
@@ -201,7 +203,7 @@ export const HERO_CON_CAMPO_BUSCADOR = `<header class="hero">
 
 export const HERO_CON_ETIQUETAS = `<header class="hero">
   <div class="content corner">
-    <div class="d-flex flex-column py-3 py-lg-5">
+    <div class="d-flex flex-column">
       <nav aria-label="Navegación secundaria">
         <ol class="breadcrumb m-0">
           <li class="breadcrumb-item">
@@ -229,7 +231,7 @@ export const HERO_CON_ETIQUETAS = `<header class="hero">
 
 export const HERO_CON_DESPLEGABLES = `<header class="hero">
   <div class="content corner">
-    <div class="d-flex flex-column py-3 py-lg-5">
+    <div class="d-flex flex-column">
       <nav aria-label="Navegación secundaria">
         <ol class="breadcrumb m-0">
           <li class="breadcrumb-item">
@@ -326,7 +328,7 @@ export const HERO_CON_DESPLEGABLES = `<header class="hero">
 export const HERO_COLORES = `<!-- default -->
 <header class="hero bg-content-hero-cyan">
   <div class="content corner">
-    <div class="d-flex flex-column py-3 py-lg-5">
+    <div class="d-flex flex-column">
       <nav aria-label="Navegación secundaria">
         <ol class="breadcrumb m-0">
           <li class="breadcrumb-item">
@@ -356,7 +358,7 @@ export const HERO_COLORES = `<!-- default -->
 <!-- sky -->
 <header class="hero bg-content-hero-sky">
   <div class="content corner">
-    <div class="d-flex flex-column py-3 py-lg-5">
+    <div class="d-flex flex-column">
       <nav aria-label="Navegación secundaria">
         <ol class="breadcrumb m-0">
           <li class="breadcrumb-item">
@@ -386,7 +388,7 @@ export const HERO_COLORES = `<!-- default -->
 <!-- yellow -->
 <header class="hero bg-content-hero-yellow">
   <div class="content corner">
-    <div class="d-flex flex-column py-3 py-lg-5">
+    <div class="d-flex flex-column">
       <nav aria-label="Navegación secundaria">
         <ol class="breadcrumb m-0">
           <li class="breadcrumb-item">
@@ -416,7 +418,7 @@ export const HERO_COLORES = `<!-- default -->
 <!-- purple -->
 <header class="hero bg-content-hero-purple">
   <div class="content corner">
-    <div class="d-flex flex-column py-3 py-lg-5">
+    <div class="d-flex flex-column">
       <nav aria-label="Navegación secundaria">
         <ol class="breadcrumb m-0">
           <li class="breadcrumb-item">
@@ -447,7 +449,7 @@ export const HERO_COLORES = `<!-- default -->
 
 export const HERO_LIGHT = `<header class="hero bg-content-hero-light">
   <div class="content corner">
-    <div class="d-flex flex-column py-3 py-lg-5">
+    <div class="d-flex flex-column">
       <nav aria-label="Navegación secundaria">
         <ol class="breadcrumb m-0">
           <li class="breadcrumb-item">
@@ -477,7 +479,7 @@ export const HERO_LIGHT = `<header class="hero bg-content-hero-light">
 
 export const HERO_DARK = `<header class="hero bg-content-hero-dark">
   <div class="content corner">
-    <div class="d-flex flex-column py-3 py-lg-5">
+    <div class="d-flex flex-column">
       <nav aria-label="Navegación secundaria">
         <ol class="breadcrumb m-0">
           <li class="breadcrumb-item">

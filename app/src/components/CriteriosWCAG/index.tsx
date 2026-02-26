8015f461ef3abbb1678e2663a6f37196981daeff
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2026-02-26 15:07:08 -0300

    docs: Lógica para descripción custom
    Aplicado en Desplegable de navegación y patrones

--- app/src/components/CriteriosWCAG/index.tsx
+++ app/src/components/CriteriosWCAG/index.tsx
@@ -3,24 +3,56 @@ import React from 'react';
 
 import criteriaItems from './criterios.json';
 
-interface CriteriaProps {
-  list: Array<string>; // uno pasa una lista de ids, porque siempre hay +1 criterio
+interface CustomDescription {
+  customId: string;
+  customDescripcion: string;
 }
 
-const CriteriosWCAG: React.FC<CriteriaProps> = ({ list }) => {
-  const filteredCriteria = list.map((listId) => criteriaItems.find(({ id }) => listId === id)); // te devuelveun array con los ids encontrados
+interface CriteriaProps {
+  list: Array<string>; // uno pasa una lista de ids, porque siempre hay +1 criterio
+  customItem?: CustomDescription[];
+}
+// list es un el array de objetos del json
+// customItem = [{}, {}]
+const CriteriosWCAG: React.FC<CriteriaProps> = ({ list, customItem }) => {
+  const filteredCriteria = list.map((listId) => criteriaItems.find(({ id }) => listId === id)); // te devuelve un array con los ids encontrados
 
-  return filteredCriteria.map((criteria) => (
+  return (
     <>
-      {/* el enlace es opcional */}
-      <a className="external" href={criteria?.url} target="_blank" rel="noopener noreferrer">
-        {criteria?.title}
-      </a>
-      {/* la descripcion tmb es opcional, a veces no va y a veces va en un componente una descripcion, y en otro, otra o mas corta */}
-      <p dangerouslySetInnerHTML={{ __html: criteria?.description || '' }}></p>{' '}
-      {/* es para  renderizar los <i></i> y <strong></strong>, sino, no se ven */}
+      {filteredCriteria.map((criteria) => {
+        // Buscar si hay una versión custom para este criterio
+        const customVersion = customItem?.find((c) => c.customId === criteria?.id);
+
+        return (
+          <React.Fragment key={criteria?.id}>
+            {/* el enlace es opcional */}
+            {criteria?.url && (
+              <a className="external" href={criteria.url} target="_blank" rel="noopener noreferrer">
+                {criteria?.title}
+              </a>
+            )}
+
+            {/* si hay una descripcion custom, la renderiza, sino el que viene del .json */}
+            {customVersion ? (
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: customVersion.customDescripcion,
+                }}
+              />
+            ) : (
+              criteria?.description && (
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: criteria.description,
+                  }}
+                />
+              )
+            )}
+          </React.Fragment>
+        );
+      })}
     </>
-  ));
+  );
 };
 
 export default CriteriosWCAG;

60d8368e06efcab0e229fe90d9f7158eccf790cd
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2026-03-05 11:13:20 -0300

    fix: Se corrige h1 y p de Hero Landking

--- app/src/documents/HeroDocs/code-views.ts
+++ app/src/documents/HeroDocs/code-views.ts
@@ -709,7 +709,7 @@ export const HERO_LANDING = `
 		<div class="panel-horizontal-content">
 			<div class="panel-horizontal">
 				<div class="panel-body">
-					<h1 class="panel-title">Título del destacado</h2>
+					<h1 class="panel-title">Título del destacado</h1>
 					<p class="panel-text text-xl">
 						Este es el cuerpo de un destacado. Debe ser breve y conciso, de pocas
 						líneas. No puede contener negritas ni enlaces. Este es el cuerpo de un

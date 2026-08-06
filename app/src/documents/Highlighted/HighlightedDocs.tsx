116508d67243b71f736f2d912715eb5f3b2bc752
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2026-08-06 11:59:53 -0300

    docs: Se agregan imgs para probar

--- app/src/documents/Highlighted/HighlightedDocs.tsx
+++ app/src/documents/Highlighted/HighlightedDocs.tsx
@@ -997,6 +997,86 @@ const HighlightedDocs: React.FC = () => {
       ),
     },
     {
+      subtitle: 'img chica con panel-media edge',
+      content: (
+        <>
+          <div className="container">
+            <ZoomContainer>
+              <div className="panel-horizontal-content panel-horizontal-container bg-gradient-blue p-0">
+                <div className="panel-horizontal panel-inverted">
+                  <div className="panel-body">
+                    <h2 className="panel-title">Título del destacado</h2>
+                    <p className="panel-text">
+                      Este es el cuerpo de un destacado. Debe ser breve y conciso, de pocas líneas. No puede contener
+                      negritas ni enlaces. Este es el cuerpo de un destacado. Debe ser breve y conciso, de pocas líneas.
+                      No puede contener negritas ni enlaces
+                    </p>
+                    <div className="panel-footer">
+                      <LinkClient className="btn btn-light btn-lg">Botón</LinkClient>
+                      <LinkClient className="btn btn-outline-light btn-lg">Botón</LinkClient>
+                    </div>
+                  </div>
+
+                  <div className="panel-media panel-media-edge">
+                    <Image
+                      // src={`${basePath}/images/highlighted/destacado_container_2_332.png`}
+                      src={`${basePath}/images/highlighted/destacado_multimedia_container.png`}
+                      // src={`${basePath}/images/highlighted/destacado_multimedia_container_353_450.png`}
+                      // src={`https://nba-back-ocp-dev.gcba.gob.ar/api/v1/imagen/banner-promocional/banner_promocional_1.png`}
+                      alt="descripción de imagen"
+                      className="panel-img "
+                      width={538}
+                      height={304}
+                    ></Image>
+                  </div>
+                </div>
+              </div>
+            </ZoomContainer>
+          </div>
+        </>
+      ),
+    },
+    {
+      subtitle: 'img grande sin panel-media edge',
+      content: (
+        <>
+          <div className="container">
+            <ZoomContainer>
+              <div className="panel-horizontal-content panel-horizontal-container bg-gradient-blue p-0">
+                <div className="panel-horizontal panel-inverted">
+                  <div className="panel-body">
+                    <h2 className="panel-title">Título del destacado</h2>
+                    <p className="panel-text">
+                      Este es el cuerpo de un destacado. Debe ser breve y conciso, de pocas líneas. No puede contener
+                      negritas ni enlaces. Este es el cuerpo de un destacado. Debe ser breve y conciso, de pocas líneas.
+                      No puede contener negritas ni enlaces
+                    </p>
+                    <div className="panel-footer">
+                      <LinkClient className="btn btn-light btn-lg">Botón</LinkClient>
+                      <LinkClient className="btn btn-outline-light btn-lg">Botón</LinkClient>
+                    </div>
+                  </div>
+
+                  <div className="panel-media ">
+                    <Image
+                      // src={`${basePath}/images/highlighted/destacado_container_2_332.png`}
+                      src={`${basePath}/images/highlighted/destacado_multimedia_container.png`}
+                      // src={`${basePath}/images/highlighted/destacado_multimedia_container_353_450.png`}
+                      // src={`https://nba-back-ocp-dev.gcba.gob.ar/api/v1/imagen/banner-promocional/banner_promocional_1.png`}
+                      alt="descripción de imagen"
+                      className="panel-img "
+                      width={538}
+                      height={304}
+                    ></Image>
+                  </div>
+                </div>
+              </div>
+            </ZoomContainer>
+          </div>
+        </>
+      ),
+    },
+    {
       subtitle: 'Invertido',
       content: (
         <>
